3c0542510da00bd093cfb2c597cd113c35a91889
Peter Macharia <mungaipeter640@gmail.com>
2023-06-16 11:18:23 +0300

    extend and include

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -6,7 +6,11 @@
     <title>Document</title>
 </head>
 <body>
-@include('nav')
+    @extends('layouts.app')
+
+    @section('content')
     <h1>Contacts </h1>
+    <h1>This is main content</h1>
+    @endsection
 </body>
 </html>
