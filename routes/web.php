d11e0031e66a4cb9e718f3381379117abcc4227d
Peter Macharia <mungaipeter640@gmail.com>
2023-06-21 14:24:20 +0300

    auth middleware

--- routes/web.php
+++ routes/web.php
@@ -29,12 +29,16 @@ Route::get('/contacts', function () {
 });
 Route::get('/login', function () {
     return view('login');
-});
+})->name('login');
+
 Route::get('/signup', function () {
     return view('signup');
 });
-Route::get('/dashboard', function () {
-    return view('dashboard');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    });
 });
 
 Route::post('/process_signup', [UserController::class, 'store']);
