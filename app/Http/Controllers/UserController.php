5ef9227b703bae3400f6a5cff4cb3b1126380f75
Peter Macharia <mungaipeter640@gmail.com>
2023-06-26 13:27:06 +0300

    user validation and exception handling

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,10 +9,10 @@ use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
-    public function store(Request $request)
-    {
+    public function store(Request $request){
+
         $request->validate([
-            'username' => 'required|min:3|alpha',
+            'username' => 'required|min:3|max:10|alpha|unique:users,username',
             'email' => ['required', 'email', Rule::unique('users', 'email')],
             'password' => [
                 'required',
@@ -22,27 +22,29 @@ class UserController extends Controller
         ], [
             'username.required' => 'Please enter a username.',
             'username.min' => 'Username must be at least :min characters long.',
+            'username.max' => 'Username must not be more than :max characters long.',
             'username.alpha' => 'Username must only contain alphabetic characters.',
+            'username.unique' => 'This username is already in use.',
             'email.required' => 'Please enter an email address.',
             'email.email' => 'Please enter a valid email address.',
-            'email.unique' => 'This email address already in use.',
+            'email.unique' => 'This email address is already in use.',
             'password.required' => 'Please enter a password.',
             'password.min' => 'Password must be at least :min characters long.',
             'password.regex' => 'Password must contain at least one uppercase letter, one lowercase letter, and one number.',
         ]);
-        try {
-            $user = $request->all();
+            try {
+                $user = $request->all();
 
-            $newUser = new User([
-                'username' => $user['username'],
-                'email' => $user['email'],
-                'password' => $user['password'],
-            ]);
-            $newUser->save();
+                $newUser = new User([
+                    'username' => $user['username'],
+                    'email' => $user['email'],
+                    'password' => $user['password'],
+                ]);
+                $newUser->save();
 
-            return redirect('login')->with('success', 'Signup successful.');
-        } catch (\Exception $e) {
-            return redirect()->back()->withInput()->withErrors(['error' => 'An error occurred while signing up. Please try again.']);
-        }
+                return redirect('login')->with('success', 'Signup successful.');
+            } catch (\Exception $e) {
+                return redirect()->back()->withInput()->withErrors(['error' => 'An error occurred while signing up. Please try again.']);
+            }
     }
 }    
