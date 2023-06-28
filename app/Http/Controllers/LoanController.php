8431683e2ee6d3c964f75055bfa4ec0cbc46c14e
Peter Macharia <mungaipeter640@gmail.com>
2023-06-28 22:05:18 +0300

    loancontroller

--- app/Http/Controllers/LoanController.php
+++ app/Http/Controllers/LoanController.php
@@ -10,16 +10,16 @@ use Illuminate\Support\Facades\Redirect;
 class LoanController extends Controller
 {
     public function store(Request $request)
-{
-    
-    $loan = new Loan();
-    $loan->username = $request->input('username');
-    $loan->email = $request->input('email');
-    $loan->amount = $request->input('amount');
-    $loan->save();
+    {
+        
+        $loan = new Loan();
+        $loan->username = $request->input('username');
+        $loan->email = $request->input('email');
+        $loan->amount = $request->input('amount');
+        $loan->save();
 
-    return redirect('dashboard/loans')->with('success', 'Application successful. Please wait for approval.');
-}
+        return redirect('dashboard/loans')->with('success', 'Application successful. Please wait for approval.');
+    }
 
 
 }
